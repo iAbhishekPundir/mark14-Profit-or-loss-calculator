fc2c0ce98fc06453304193a7b0ac80f9f108d856
Abhishek <pundirabhishek987@gmail.com>
2022-11-29 23:45:27 +0530

    Fix Bug

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ checkBtn.addEventListener("click", clickHandler);
 function calculateProfitLoss(initialPrice, noOfStocks, finalPrice) {
     if (finalPrice > initialPrice) {
         var profit = (finalPrice - initialPrice) * noOfStocks;
-        var profitPercent = (profit/initialPrice) * 100;
+        var profitPercent = ((profit/(initialPrice*noOfStocks)) * 100).toFixed(2);
         result.innerText = `Congrats you made a profit of ${profit} and the profit percent is ${profitPercent}%.`
         result.style.color = "green";
     } else
     if (initialPrice > finalPrice) {
         var loss = (initialPrice - finalPrice) * noOfStocks;
-        var lossPercent = (loss/initialPrice) * 100;
+        var lossPercent = ((loss/(initialPrice*initialPrice)) * 100).toFixed(2);
         result.innerText = `Oops! You made a loss of ${loss} and the loss percent is ${lossPercent}%.`
         result.style.color = "red";
     } else {
@@ -29,11 +29,11 @@ function clickHandler() {
     var stocks = Number(noOfStocks.value);
     var fPrice = Number(finalPrice.value);
     if (iPrice == "" || iPrice <=0) {
-        result.innerText = "Enter Initial Price and should be > 0"
+        result.innerText = "Enter all the fields and should be > 0"
     } else if (stocks == "" || stocks <=0 ) {
-        result.innerText = "Enter Number of Stocks and should be > 0 "
+        result.innerText = "Enter all the fields and should be > 0 "
     } else if (fPrice == "" || fPrice <=0) {
-        result.innerText = "Enter Final Price and should be > 0"
+        result.innerText = "Enter all the fields and should be > 0"
     } else {
         calculateProfitLoss(iPrice, stocks, fPrice);
     }
